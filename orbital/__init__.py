9a99b52b57a21081f1c83abec870e866e2b07d41
mprib <prible@gmail.com>
2023-01-29 10:52:53 -0600

    fix version and author references

--- orbital/__init__.py
+++ orbital/__init__.py
@@ -1,11 +1,11 @@
 """Top-level package for basic_template_repo."""
 
 __package_name__ = "orbital"
-__version__ = "v2023.01.1006"
+__version__ = "0.0.0"
 
-__author__ = """Skelly FreeMoCap"""
-__email__ = "info@freemocap.org"
-__repo_owner_github_user_name__ = "freemocap"
+__author__ = """Mac Prible"""
+__email__ = "prible@gmail.com"
+__repo_owner_github_user_name__ = "mprib"
 __repo_url__ = f"https://github.com/{__repo_owner_github_user_name__}/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"
 
